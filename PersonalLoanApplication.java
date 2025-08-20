1f9d55e0800eed8012eac548014714f34aadb0e8
nikitakadam2293 <nikitakadam2293@gmail.com>
2025-08-20 10:36:27 +0530

    Add files via upload
    adds

---
+++ PersonalLoanApplication.java
@@ -0,0 +1,13 @@
+package com.personal_loan.personal_loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalLoanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalLoanApplication.class, args);
+	}
+
+}
